d3edc9db1089134d391e3138ce59e4ad2331334e
ffabi <ffabi1997@gmail.com>
2018-06-05 15:58:19 +0200

    __receive_message kiszervezve

--- chatserver/ChatManager.py
+++ chatserver/ChatManager.py
@@ -15,7 +15,7 @@ class ChatManager:
         self.__server_socket.bind((socket.gethostname(), 12345))
         self.__server_socket.listen(5)
         self.__timeout = 300
-        
+    
     
     def run(self):
         """
@@ -53,30 +53,12 @@ class ChatManager:
         
         hello_done = False
         while True:
-            try:
-                received = connection.recv(1024)
-            except socket.timeout:
-                logging.warning("Connection timeout")
-                connection.send(Protocol.server_message("Connection timeout"))
-                connection.close()
+            
+            valid, command, message = self.__receive_message(connection)
+            
+            if not valid:
                 break
-            except ConnectionResetError:
-                logging.warning("Connection closed by client")
-                break
-    
-    
-            if len(received) == 0:
-                logging.warning("Connection unexpectedly closed or no byte received")
-                connection.send(Protocol.server_message("Whatever happened, you cannot receive this message :("))
-                connection.close()
-                break
-    
-            command = received[0]
-            terminator = received[-1:]
-    
-            if terminator != bytes([Protocol.Flags.TERMINATOR]):
-                logging.warning("Terminator byte not received")
-    
+            
             if not hello_done:
                 if command == Protocol.Flags.HELLO:
                     logging.info("HELLO message received")
@@ -86,30 +68,60 @@ class ChatManager:
                     logging.warning("No HELLO received, closing connection")
                     connection.close()
                     break
-    
+            
             else:
                 if command == Protocol.Flags.HELLO:
                     logging.warning("HELLO message received again")
                     connection.send(Protocol.hello_message())
-        
+                
                 # TODO
                 if command == Protocol.Flags.LOGIN:
                     logging.info("LOGIN message received")
-        
+                
                 elif command == Protocol.Flags.LOGOUT:
                     logging.info("LOGOUT message received")
-        
+                
                 elif command == Protocol.Flags.USER:
                     logging.info("USER message received")
-        
+                
                 elif command == Protocol.Flags.EXIT:
                     logging.info("EXIT message received, connection closed")
                     connection.send(Protocol.server_message("See you later"))
                     connection.close()
                     break
-        
+                
                 elif command == Protocol.Flags.SERVER:
                     logging.warning("Server received SERVER message, connection closed")
                     connection.send(Protocol.server_message("Did u just try to send a server message to the server? XD"))
                     connection.close()
-
+    
+    @staticmethod
+    def __receive_message(connection: socket.socket):
+        
+        try:
+            received = connection.recv(Protocol.max_message_size)
+        
+        except socket.timeout:
+            logging.warning("Connection timeout")
+            connection.send(Protocol.server_message("Connection timeout"))
+            connection.close()
+            return False
+        
+        except ConnectionResetError:
+            logging.warning("Connection closed by client")
+            return False
+        
+        if len(received) == 0:
+            logging.warning("Connection unexpectedly closed or no byte received")
+            connection.send(Protocol.server_message("Whatever happened, you probably cannot receive this message :("))
+            connection.close()
+            return False
+        
+        command = received[0]
+        message = received[1:-1]
+        terminator = received[-1:]
+        
+        if terminator != bytes([Protocol.Flags.TERMINATOR]):
+            logging.warning("Terminator byte not received")
+        
+        return True, command, message
